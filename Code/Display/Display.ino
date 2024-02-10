d73a228b141c1b2c0ac8af3dce6dc208096ad1ae
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2024-02-10 10:49:32 +0100

    Display

--- Code/Display/Display.ino
+++ Code/Display/Display.ino
@@ -24,18 +24,17 @@
 */
 
 #include <TFT_eSPI.h> // Hardware-specific library
-#include <SPI.h>
 #include "Free_Fonts.h" // Include the header file attached to this sketch
-#include <RotaryEncoder.h>
-#include <debounce.h>
+#include "WiFi.h"
+#include "AudioTools.h"
+#include "AudioCodecs/CodecMP3Helix.h"
 
-// Rotary encoder variables
-const int rotaryMin = 0;                    // Set rotary encoder min, max and steps var
-const int rotaryMax = 50;
-const int rotarySteps = 2;
-const int zone3RotaryIn1 = 25;
-const int zone3RotaryIn2 = 26;
-const int zone3ButtonPin = 27;
+const char *urls[] = {
+    "http://icecast.vrtcdn.be/mnm-high.mp3"};
+
+// Wifi
+const char *wifi = "Boshoek44c-verdiep-2.4GHz";
+const char *password = "Boshoek44c1";
 
 // Zone Variables
 int zone1_vol = 0;
@@ -45,27 +44,26 @@ bool zone1_status = false;
 bool zone2_status = false;
 bool zone3_status = false;
 
-void buttonHandler(uint8_t btnId, uint8_t btnState) {
-  if (btnState == BTN_PRESSED) {
-    if (btnId == 3){
-      setZoneStatus(3, !zone3_status);
-    }
-  }
-}
-
 TFT_eSPI tft = TFT_eSPI();                   // Invoke custom library with default width and height
-RotaryEncoder *zone3Encoder = nullptr;           // A pointer to the dynamic created rotary encoder instance. This will be done in setup
-static Button zone3Button(3, buttonHandler);   // button with id 3
-
-IRAM_ATTR void zone3CheckPosition()
-{
-  zone3Encoder->tick();
-}
+URLStream urlStream(wifi, password);
+AudioSourceURL source(urlStream, urls, "audio/mp3");
+I2SStream i2s;
+MP3DecoderHelix decoder;
+AudioPlayer player(source, i2s, decoder);
 
 void setup(void) {
   // Serial setup
   Serial.begin(115200);
 
+  // Audio setup
+  auto config = i2s.defaultConfig(TX_MODE);
+  config.pin_bck = 0;
+  config.pin_ws = 10;
+  config.pin_data = 11;
+  i2s.begin(config);
+
+  // setup player
+  player.begin();
 
   // TFT setup
   tft.begin();
@@ -79,7 +77,7 @@ void setup(void) {
   tft.drawString("SOURCE: INTERNET RADIO", 239, 10, 4);
   // Header radio station
   tft.setTextColor(TFT_MAGENTA, TFT_BLUE);
-  tft.drawString("ZENDER: RADIO 2 ANT", 239, 50, 4);
+  tft.drawString("ZENDER: MNM", 239, 50, 4);
 
 
   // Zones 
@@ -108,43 +106,15 @@ void setup(void) {
   tft.setTextDatum(TR_DATUM);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
 
-
-  // Rotary encoder setup
-  zone3Encoder = new RotaryEncoder(zone3RotaryIn1, zone3RotaryIn2, RotaryEncoder::LatchMode::TWO03);
-  attachInterrupt(digitalPinToInterrupt(zone3RotaryIn1), zone3CheckPosition, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(zone3RotaryIn2), zone3CheckPosition, CHANGE);
-
-  // Button setup
-  pinMode(zone3ButtonPin, INPUT_PULLUP);
+  // TFT Backlight
+  pinMode(32, OUTPUT);
+  digitalWrite(32, HIGH);
 }
 
-void loop() {
-  // Button stuff
-  pollButtons();
-
-  // Rotary encoder stuff
-  int zone3Position = zone3Encoder->getPosition();
-  if(zone3Position > rotaryMax){
-    zone3Encoder->setPosition(rotaryMax);
-    zone3Position = rotaryMax;
-  }
-  else if(zone3Position < rotaryMin){
-    zone3Encoder->setPosition(rotaryMin);
-    zone3Position = rotaryMin;
-  }
-  else{
-    // Serial.print("Position");
-    // Serial.println(zone3Position * rotarySteps);
-    if (zone3_vol != zone3Position * rotarySteps){
-      Serial.println("Setting zone volume");
-      setZoneVolume(3, zone3Position * rotarySteps);
-      zone3_vol = zone3Position * rotarySteps;
-    }
-  }
-}
-
-void pollButtons() {
-  zone3Button.update(digitalRead(zone3ButtonPin));
+void loop() { 
+  // updateVolume(); // remove comments to activate volume control
+  // updatePosition();  // remove comments to activate position control
+  player.copy();
 }
 
 void setZoneStatus(int zone, bool status){
